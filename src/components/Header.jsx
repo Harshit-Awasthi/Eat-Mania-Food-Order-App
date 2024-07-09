50865777a9ff347fa2680625cbbdd9ad51fb7d70
Harshit Awasthi <awasthiharshit8@gmail.com>
2024-07-09 14:05:37 +0530

    "Opening the Cart in the Modal via a new Context"

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,13 +2,19 @@ import Button from './UI/Button.jsx';
 import logoImg from '../assets/logo.jpg';
 import CartContext from '../store/CartContext.jsx';
 import { useContext } from 'react';
+import UserProgressContext from '../store/UserProgressContext.jsx';
 
 export default function Header(){
     const cartCtx=useContext(CartContext);
+    const userProgressCtx=useContext(UserProgressContext);
 
     const totalCartItems=cartCtx.items.reduce((totalNumberOfItems,item)=>{
         return totalNumberOfItems + item.quantity;
     },0);
+
+    function handleShowCart(){
+        userProgressCtx.showCart();
+    }
 
     return <header  id="main-header">
         <div id="title">
@@ -16,7 +22,7 @@ export default function Header(){
             <h1>Eat-Mania</h1>
         </div>
         <nav>
-            <Button textOnly={true}>
+            <Button textOnly={true} onClick={handleShowCart}>
                 Cart ({totalCartItems})
             </Button>
         </nav>
